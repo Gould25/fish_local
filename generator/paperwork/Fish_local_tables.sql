135414733046aeaeb5ef753461a3076519430246
jg7f9 <jg7f9@msy.edu>
2016-11-21 09:21:18 -0600

    Split sql for database into 2 files 1 for table creation and 1 for insertion

--- generator/paperwork/Fish_local_tables.sql
+++ generator/paperwork/Fish_local_tables.sql
@@ -13,7 +13,7 @@ CREATE TABLE if NOT EXISTS fish(
 );
 
 /*Creating table Location */
-Create TABLE if NOT EXISTS location(
+CREATE TABLE if NOT EXISTS location(
   lat Float(10,6) NOT NULL,
   lng Float(10,6) NOT NULL,
   b_o_w VARCHAR(25) NOT NULL,
@@ -25,10 +25,34 @@ Create TABLE if NOT EXISTS location(
   ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-/*Creating table */
+/*Creating table record */
+CREATE TABLE IF NOT EXISTS record(
+  fishName VARCHAR(25) NOT NULL,
+  b_o_w VARCHAR(25) NOT NULL,
+  wght INTEGER(4) NOT NULL,
+  length INTEGER(4),
+  line_wght INTEGER(3) NOT NULL,
+  FOREIGN KEY (fishName) REFERENCES fish (fishName)
+  ON UPDATE CASCADE ON DELETE CASCADE
+  /*FOREIGN KEY (b_o_w) REFERENCES location (b_o_w)
+  ON UPDATE CASCADE ON DELETE CASCADE*/
+);
 
+/*Creating table family */
+CREATE TABLE IF NOT EXISTS family(
+  fishName VARCHAR(25) NOT NULL,
+  familyName VARCHAR(25) NOT NULL,
+  FOREIGN KEY (fishName) REFERENCES fish (fishName)
+  ON UPDATE CASCADE ON DELETE CASCADE
+);
 
-
+/*Creating table alias */
+CREATE TABLE IF NOT EXISTS alias(
+  fishName VARCHAR(25) NOT NULL,
+  aliasName VARCHAR(25) NOT NULL,
+  FOREIGN KEY (fishName) REFERENCES fish (fishName)
+  ON UPDATE CASCADE ON DELETE CASCADE
+);
 
 /* ***** INSERTING DATA ***** */
 INSERT INTO fish(fishName,ave_wght,picture)
@@ -42,4 +66,16 @@ INSERT INTO location(lat,lng,b_o_w,state,county,fishName)
           "Hickory","Channel Catfish"),
         (37.837075,-93.262909,"Pomme de Terre","MO",
           "Hickory","Blue Catfish")
+;
+
+INSERT INTO record(fishName, b_o_w, wght, length, line_wght)
+VALUES ("Channel Catfish", "Pomme de Terre", 57, NULL, 30)
+;
+
+INSERT INTO family(fishName, familyName)
+VALUES ("Channel Catfish", "Catfish")
+;
+
+INSERT INTO alias(fishName, aliasName)
+VALUES ("Channel Catfish", "Mr Whiskers")
 ;
