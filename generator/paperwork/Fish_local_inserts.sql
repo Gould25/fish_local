efdf47198c915abd6127838a45df3feca278755f
jg7f9 <jg7f9@msy.edu>
2016-11-26 01:20:05 -0600

    Updated DB entries, added bait view

--- generator/paperwork/Fish_local_inserts.sql
+++ generator/paperwork/Fish_local_inserts.sql
@@ -3,14 +3,30 @@ INSERT INTO fish(fishName,ave_wght,picture)
 VALUES ("Channel Catfish", "2-12 lbs","/images/Channel-Catfish.jpg"),
        ("Blue Catfish", "35-135 lbs","/images/Blue-Catfish.jpg"),
        ("Flathead Catfish", "2-20 lbs","/images/Flathead-Catfish.jpg"),
-       ("Black Bullhead Catfish", "<2 lbs","/image/bullhead_catfish.jpg"),
-       ("Largemouth Bass","0.5-4.5","/image/largemaouth-bass.png")
+       ("Bullhead Catfish", "<2 lbs","/image/bullhead_catfish.jpg"),
+       ("Largemouth Bass","0.5-4.5","/image/largemaouth-bass.jpg"),
+       ("White Bass", "0.25-1.25","/images/white-bass.jpg"),
+       ("Striped Bass", "5-20", "/images/striped-bass.jpg"),
+       ("Hybrid Striped Bass", "7-10", "/images/hybrid-striped-bass.jpg"),
+       ("Smallmouth Bass", "0.5-4", "/images/smallmouth-bass.jpg"),
+       ("Kentucky Bass", "0.5-3.5", "/images/kentucky-bass.jpg"),
+       ("Black Crappie", "<4", "/images/black-crappie.jpg"),
+       ("White Crappie", "7-10", "/images/white-crappie.jpg"),
+       ("Goggle-Eye", "1", "/images/goggle-eye.jpg"),
+       ("Muskellunge", "39", "/images/muskellunge.jpg"),
+       ("Northern Pike", "40+", "/images/norther-pike.jpg"),
+       ("Spoonbill", "160", "/images/spoonbill.jpg"),
+       ("Brown Trout", "10", "/images/brown-trout.jpg"),
+       ("Rainbow Trout", "1.5", "/images/rainbow-trout.jpg"),
+       ("Walleye", "8", "/images/walleye.jpg"),
+       ("Bigmouth Buffalo", "65", "/images/bigmouth-buffalo.jpg"),
+       ("Common Carp", "8", "/images/common-carp.jpg"),
+       ("Grass Carp", "25", "/images/grass-carp.jpg"),
+       ("Bluegill", "0.75", "/images/bluegill.jpg")
 ;
-INSERT INTO location(lat,lng,b_o_w,state,county,fishName)
+INSERT INTO location(lat,lng,b_o_w,state,county)
  VALUES (37.837075,-93.262909,"Pomme de Terre","MO",
-          "Hickory","Channel Catfish"),
-        (37.837075,-93.262909,"Pomme de Terre","MO",
-          "Hickory","Blue Catfish")
+          "Hickory")
 ;
 
 INSERT INTO record(fishName, b_o_w, wght, length, line_wght)
@@ -18,7 +34,31 @@ VALUES ("Channel Catfish", "Pomme de Terre", 57, NULL, 30)
 ;
 
 INSERT INTO family(fishName, familyName)
-VALUES ("Channel Catfish", "Catfish")
+VALUES ("Channel Catfish", "Catfish"),
+        ("Blue Catfish", "Catfish"),
+        ("Flathead Catfish", "Catfish"),
+        ("Bullhead Catfish", "Catfish"),
+        ("Largemouth Bass", "Bass"),
+        ("White Bass", "Bass"),
+        ("Striped Bass", "Bass"),
+        ("Smallmouth Bass", "Bass"),
+        ("Hybrid Striped Bass", "Bass"),
+        ("Kentucky Bass", "Bass"),
+        ("White Crappie", "Panfish"),
+        ("White Crappie", "Panfish"),
+        ("Goggle-Eye", "Panfish"),
+        ("Muskellunge", "Pikes"),
+        ("Northern Pike", "Pikes"),
+        ("Spoonbill", "Sturgeon"),
+        ("Brown Trout", "Trout"),
+        ("Rainbow Trout", "Trout"),
+        ("Walleye", "Perch"),
+        ("Bigmouth Buffalo", "Sucker"),
+        ("Common Carp", "Minnow"),
+        ("Grass Carp", "Minnow"),
+        ("Bluegill", "Panfish")
+
+
 ;
 
 INSERT INTO alias(fishName, aliasName)
@@ -28,25 +68,51 @@ VALUES ("Channel Catfish", "Mr Whiskers"),
 
 INSERT INTO bait(fishName, baitName, description)
 VALUES ("Channel Catfish", "Nightcrawlers","typical large earthworm"),
-        ("Largemouth Bass", "Rappala","2 hook minnow")
+        ("Channel Catfish", "Chicken Liver","raw"),
+        ("Channel Catfish", "Stink bait","blood, cheese, ground liver"),
+        ("Channel Catfish", "Cut Shad","Whole"),
+        ("Channel Catfish", "Hotdog","soaked in grape coolaid"),
+        ("Blue Catfish", "Nightcrawlers","typical large earthworm"),
+        ("Blue Catfish", "Chicken Liver","raw"),
+        ("Blue Catfish", "Stink bait","blood, cheese, ground liver"),
+        ("Blue Catfish", "Cut Shad","Whole"),
+        ("Blue Catfish", "Hotdog","soaked in grape coolaid"),
+        ("Blue Catfish", "Chicken","Whole fryer soaked in garlic and oil"),
+        ("Flathead Catfish", "Nightcrawlers","typical large earthworm"),
+        ("Flathead Catfish", "Chicken Liver","raw"),
+        ("Flathead Catfish", "Stink bait","blood, cheese, ground liver"),
+        ("Flathead Catfish", "Cut Shad","Whole"),
+        ("Largemouth Bass", "Rappala","2 hook minnow"),
+        ("Largemouth Bass", "Rubber Worm","slip weight rig")
+
 ;
 
 INSERT INTO artificial(baitName,size,color)
-VALUES  ("Rappala", "6inch","Black/Silver")
+VALUES  ("Rappala", "6inch","Black/Silver"),
+        ("Rubber Worm", "6 inch", "Black"),
+        ("Rubber Worm", "6 inch", "Black/Silver"),
+        ("Rubber Worm", "6 inch", "Shortreuse"),
+        ("Rubber Worm", "6 inch", "Oil can"),
+        ("Rubber Worm", "6 inch", "Purple")
 ;
 
 INSERT INTO organic(baitName)
-VALUES  ("Nightcrawlers")
+VALUES  ("Nightcrawlers"),
+        ("Chicken Liver"),
+        ("Stink bait"),
+        ("Cut Shad"),
+        ("Hotdog"),
+        ("Chicken")
 ;
 
-INSERT INTO boat(lat,lng)
-VALUES  (37.837075,-93.262909)
+INSERT INTO boat(loc_id)
+VALUES  (1)
 ;
 
-INSERT INTO bank(lat,lng)
-VALUES  (37.837075,-93.262909)
+INSERT INTO bank(loc_id)
+VALUES  (1)
 ;
 
-INSERT INTO fishlog(fishName,b_o_w,notes)
-VALUES  ("channel Catfish", "Pomme de Terre", "Trial log sample")
+INSERT INTO fishlog(fishName,loc_id,wght,length,baitName,notes)
+VALUES  ("Channel Catfish", 1, 2, 22,"Nightcrawlers","sunset on bottom")
 ;
--- generator/paperwork/Fish_local_tables.sql
+++ generator/paperwork/Fish_local_tables.sql
@@ -14,15 +14,13 @@ CREATE TABLE if NOT EXISTS fish(
 
 /*Creating table Location */
 CREATE TABLE if NOT EXISTS location(
+  loc_id INTEGER(3) NOT NULL AUTO_INCREMENT,
   lat Float(10,6) NOT NULL,
   lng Float(10,6) NOT NULL,
   b_o_w VARCHAR(25) NOT NULL,
   state VARCHAR(25) NOT NULL,
   county VARCHAR(25) NOT NULL,
-  fishName VARCHAR(25) NOT NULL,
-  PRIMARY KEY (lat,lng,fishName,b_o_w),
-  FOREIGN KEY (fishName) REFERENCES fish (fishName)
-  ON UPDATE CASCADE ON DELETE CASCADE
+  PRIMARY KEY (loc_id)
 );
 
 /*Creating table record */
@@ -32,8 +30,7 @@ CREATE TABLE IF NOT EXISTS record(
   wght INTEGER(4) NOT NULL,
   length INTEGER(4),
   line_wght INTEGER(3) NOT NULL,
-  CONSTRAINT FOREIGN KEY (fishName) REFERENCES fish (fishName)
-  ON UPDATE CASCADE ON DELETE CASCADE
+  FOREIGN KEY (fishName) REFERENCES fish (fishName)
 );
 
 /*Creating table family */
@@ -41,7 +38,6 @@ CREATE TABLE IF NOT EXISTS family(
   fishName VARCHAR(25) NOT NULL,
   familyName VARCHAR(25) NOT NULL,
   FOREIGN KEY (fishName) REFERENCES fish (fishName)
-  ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 /*Creating table alias */
@@ -49,7 +45,6 @@ CREATE TABLE IF NOT EXISTS alias(
   fishName VARCHAR(25) NOT NULL,
   aliasName VARCHAR(25) NOT NULL UNIQUE,
   FOREIGN KEY (fishName) REFERENCES fish (fishName)
-  ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 /*Creating table bait */
@@ -57,9 +52,8 @@ CREATE TABLE if NOT EXISTS bait(
   fishName VARCHAR(25) NOT NULL,
   baitName VARCHAR(25) NOT NULL,
   description TEXT NOT NULL,
-  PRIMARY KEY (baitName),
+  PRIMARY KEY (baitName,fishName),
   FOREIGN KEY (fishName) REFERENCES fish (fishName)
-  ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 /*Creating table Artificial */
@@ -67,7 +61,7 @@ CREATE TABLE if NOT EXISTS artificial(
   baitName VARCHAR(25) NOT NULL,
   size VARCHAR(15),
   color VARCHAR(15),
-  PRIMARY KEY (baitName)
+  PRIMARY KEY (baitName,size,color)
 );
 
 /*Creating table Organic */
@@ -78,28 +72,30 @@ CREATE TABLE if NOT EXISTS organic(
 
 /*Creating table boat */
 CREATE TABLE if NOT EXISTS boat(
-  lat Float(10,6) NOT NULL,
-  lng Float(10,6) NOT NULL,
-  PRIMARY KEY (lat,lng)
+  loc_id INTEGER(3) NOT NULL,
+  PRIMARY KEY (loc_id)
 );
 
 /*Creating table bank*/
 CREATE TABLE if NOT EXISTS bank(
-  lat Float(10,6) NOT NULL,
-  lng Float(10,6) NOT NULL,
-  PRIMARY KEY (lat,lng)
+  loc_id INTEGER(3) NOT NULL,
+  PRIMARY KEY (loc_id)
 );
 
 /*Creating table fishlog */
 CREATE TABLE IF NOT EXISTS fishlog(
   logNum INTEGER NOT NULL AUTO_INCREMENT,
   fishName VARCHAR(25) NOT NULL,
-  b_o_w VARCHAR(25) NOT NULL,
+  loc_id INTEGER(3),
   wght INTEGER(4),
   length INTEGER(4),
-  line_wght INTEGER(3),
+  baitName VARCHAR(25),
   notes TEXT,
   PRIMARY KEY (logNum),
-  CONSTRAINT FOREIGN KEY (fishName) REFERENCES fish (fishName)
-  ON UPDATE CASCADE ON DELETE CASCADE
+  FOREIGN KEY (fishName) REFERENCES fish (fishName),
+  FOREIGN KEY (baitName) REFERENCES bait (baitName)
 );
+
+/* adding foreign key loc_id  to fishlog */
+ALTER TABLE fishlog ADD FOREIGN KEY (loc_id) REFERENCES location (loc_id)
+;
