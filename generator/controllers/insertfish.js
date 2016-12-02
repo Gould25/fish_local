b56c57d5faf1bdfbd4ea018c99ec3cbb4dcb7f5a
gould25 <jg7f9@msy.edu>
2016-12-02 12:58:13 -0600

    up date

--- generator/controllers/insertfish.js
+++ generator/controllers/insertfish.js
@@ -3,5 +3,5 @@ var router = express.Router();
 var mysql = require('mysql');
 var db = require('./../controllers/db.js');
 
-  db.query('insert into fish (fishName, ave_wght, picture) VALUES (fish.fishName, fish.ave_wght, fish.picture)')
-;
+//  db.query('insert into fish (fishName, ave_wght, picture) VALUES (fish.fishName, fish.ave_wght, fish.picture)')
+//;
