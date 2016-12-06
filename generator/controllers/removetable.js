f1e89a6325a4c9cdccd1f32cef333cec5feee860
gould25 <jg7f9@msy.edu>
2016-12-06 14:03:23 -0600

    fixed table delelete

--- generator/controllers/removetable.js
+++ generator/controllers/removetable.js
@@ -4,25 +4,18 @@ function deletetable() { }
 
 // Insert location
 deletetable.prototype.deletetable = function(req, callback) {
-  if (req.body.table == 'fish'){
+  if (req.body.table == 'yes'){
+    db.query('drop table fishlog',callback);
     db.query('drop table family',callback);
     db.query('drop table alias',callback);
     db.query('drop table record',callback);
     db.query('drop table fish',callback);
-
-
-  }
-  if (req.body.table == 'location'){
     db.query('drop table location',callback);
-  }
-  if (req.body.table == 'bait'){
     db.query('drop table organic',callback);
     db.query('drop table artificial',callback);
     db.query('drop table bait',callback);
-
-  }
-  if (req.body.table == 'fishlog'){
-    db.query('drop table fishlog',callback);
+  }else{
+    res.render('insert_error');
   }
 }
 
