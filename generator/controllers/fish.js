06eeccc0f32bbee6947d9359c0f606565b03ff5d
Jacques <jacquesaronius@gmail.com>
2016-12-05 18:20:56 -0600

    Made fish insert transactional

--- generator/controllers/fish.js
+++ generator/controllers/fish.js
@@ -11,10 +11,45 @@ Fish.prototype.insert = function(req, callback) {
     path = req.file.path.replace('public', '');
 
   // Add entry to database
-  db.query('insert into fish (fishName, ave_wght, picture) VALUES (?, ?, ?)', 
-    [req.body.fishName, req.body.wght, path], callback);
+  db.beginTransaction(function() {
+    db.query('insert into fish (fishName, ave_wght, picture) VALUES (?, ?, ?)', 
+      // Write fish record
+      [req.body.fishName, req.body.wght, path], function(err) {
+      if (err) {
+        console.log(err);
+        db.rollback();
+        callback(err);
+      } else  {
+        // Write family record
+        db.query('insert into family (fishName, familyName) VALUES (?, ?)', 
+          [req.body.fishName, req.body.familyName], function(err) {
+          if (err) {
+            console.log(err);
+            db.rollback();
+            callback(err);
+          } else {
+            if (req.body.aliasName != '') {
+              db.query('insert into alias (fishName, aliasName) VALUES (?, ?)', 
+                [req.body.fishName, req.body.aliasName], function(err) {
+                if (err) {
+                  console.log(err);
+                  db.rollback();
+                  callback(err);
+                } else {
+                  db.commit();
+                  callback(null);
+                }
+              });
+            } else {
+              db.commit();
+              callback(null);
+            }
+          } 
+        });
+      }
+    });
+  });
 }
-
 module.exports = new Fish();
 
 
--- generator/routes/addfish.js
+++ generator/routes/addfish.js
@@ -4,8 +4,6 @@ var mysql = require('mysql');
 var bodyParser = require('body-parser');
 var db = require('./../controllers/db.js');
 var fish = require('./../controllers/fish.js');
-var alias = require('./../controllers/alias.js');
-var family = require('./../controllers/family.js');
 var multer = require('multer');
 var upload = multer({ dest: 'public/images/fish_uploads/' });
 
@@ -15,38 +13,16 @@ router.get('/', function(req, res, next){
 
 
 router.post('/', upload.single("picture"), function(req, res){
-    console.log(req.body);
-    console.log(req.file);
-    fish.insert(req, function(err) {
-      if (err) {
-        console.log(err);
+  console.log(req.body);
+  console.log(req.file);
+  fish.insert(req, function(err) {
+    if (err) {
+      console.log(err);
         res.render('insert_error');
       } else  {
-        family.insert(req, function(err) {
-          if (err) {
-            console.log(err);
-            res.render('insert_error');
-          } else {
-            if (req.body.aliasName != '') {
-              alias.insert(req, function(err) {
-                if (err) {
-                  console.log(err);
-                  res.render('insert_error');
-                }
-                else
-                  res.render('insert_message');
-              });
-            } else {
-              res.render('insert_message');
-            }
-          }
-        });
+        res.render('insert_message');
       }
-    });
+  });
 });
-
-
-
-
 
 module.exports = router;
